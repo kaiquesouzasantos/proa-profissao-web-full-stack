e25d40c26af511e204edc3313f195c6c95a31e08
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2024-03-01 16:00:37 -0300

    Add files via upload

---
+++ modulos/front-end/projetos/landing-page-moveis/assets/js/index.js
@@ -0,0 +1,26 @@
+ScrollReveal().reveal('.animation-right', {
+    delay: 200,
+    distance: '150px',
+    origin: 'right',
+    easing: 'ease-in-out',
+    interval: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.animation-left', {
+    delay: 200,
+    distance: '150px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    interval: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.animation-bottom', {
+    delay: 200,
+    distance: '150px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    interval: 500,
+    reset: true
+});

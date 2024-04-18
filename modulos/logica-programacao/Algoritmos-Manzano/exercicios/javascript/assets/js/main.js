e94624d79216f19c703beb101bbc2a110fd78b84
Kaique Souza Santos <kaiquesouzasantos90@gmail.com>
2024-04-17 22:17:56 -0300

    FETCH(Logica com Javascript)

--- modulos/logica-programacao/Algoritmos-Manzano/exercicios/javascript/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-import { Manipulator } from "./module/service/manipulator.service.js"
-import { exercises } from "./module/data/exercises.data.js"
-
-window.addEventListener("load", () => {
-    exercises.forEach(
-        exercise => Manipulator.addComponent(exercise)
-    )
-})
--- modulos/logica-programacao/Algoritmos-Manzano/exercicios/javascript/assets/js/module/service/DOM.service.js
+++
@@ -1,9 +0,0 @@
-export class DOM {
-    static getById(id) {
-        return document.getElementById(id)
-    }
-
-    static getContainer() {
-        return document.querySelector("[data-container]")
-    }
-}
